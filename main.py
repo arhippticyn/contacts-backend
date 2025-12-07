bcf98e587091a7d5a5917167eeab2afb6bba049c
Exohspoer <kakashihatake6hokagegygg@gmail.com>
2025-12-07 20:27:41 +0200

    create backend

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, Depends
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from db import get_db, Contact
+from sqlalchemy.orm import Session
+from models import ContactsCreate, ContactsResponse
+
+
+app = FastAPI()
+
+origins = [
+    'http://localhost:5173'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
+
+@app.post('/add')
+def create_contact(user: ContactsCreate, db: Session = Depends(get_db)):
+    # db_contact = ContactsResponse(user.name, user.Number_tel)
+    new_contact = Contact(**user.model_dump())
+    db.add(new_contact)
+    db.commit()
+    db.refresh(new_contact)
+    return new_contact
+
+@app.get('/contacts', response_model=list[ContactsResponse])
+def get_contacts(db: Session = Depends(get_db)):
+    return db.query(Contact).all()
+
+@app.delete('/delete/{contact_id}')
+def delete_contacts(contact_id: int, db: Session = Depends(get_db)):
+    contact = db.query(Contact).filter(Contact.id == contact_id).first()
+    
+    db.delete(contact)
+    db.commit()
+    
+    return {'message': 'Contact is delete'}
--- models.py
+++ models.py
@@ -2,11 +2,11 @@ from pydantic import BaseModel, Field, field_validator
 
 class ContactsCreate(BaseModel):
     name: str = Field(..., min_length=3, max_length=100, description="Your name")
-    Number_tel: str = Field(..., min_length=1, max_length=12, description="Number tel")
+    tel: str = Field(..., min_length=1, max_length=12, description="Number tel")
     
 class ContactsResponse(ContactsCreate):
     id: int
     
-    class Config():
+    class Config:
         orm_mode = True
     
